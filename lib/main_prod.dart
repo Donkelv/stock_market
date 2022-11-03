851bf6b71848eeb9dbc9c8d5bd85d8b175692137
Kevin <kevinohiro@gmail.com>
2022-11-03 18:27:50 +0100

    ADD: added production flavor

---
+++ lib/main_prod.dart
@@ -0,0 +1,13 @@
+import 'package:stock_market/app/app.dart';
+import 'package:stock_market/data/utils/exports.dart';
+import 'package:stock_market/environment.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await ScreenUtil.ensureScreenSize();
+  FlavorConfig(
+      flavor: Flavor.production,
+      values: FlavorValues(accessKey: "06eec0d8920598014b2b3322ce746800"));
+
+  runApp(const NavigatorPath());
+}
